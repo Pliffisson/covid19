4991d226ee9c52f90ab3f4ab9364f6b0bf8192d9
Pliffisson <pliffissongomes@gmail.com>
2020-04-14 20:58:03 -0400

    install material-ui

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'
 import styles from './Cards.module.css'
 
-const Cards = ({ confirmed, recovered, deaths, lastUpdate}) => {
-    if(!confirmed){
-        return 'Loading ...'
-    }
+const Cards = (props) => {
 
     return(
         <div className={styles.container}>
@@ -13,7 +10,7 @@ const Cards = ({ confirmed, recovered, deaths, lastUpdate}) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>Infected</Typography>
-                        <Typography variant='h5'>{confirmed.value}</Typography>
+                        <Typography variant='h5'>real valor</Typography>
                         <Typography color='textSecodary'>REAL DATA</Typography>
                         <Typography variant='body2'>Number of active cases of COVID-19</Typography>
                     </CardContent>

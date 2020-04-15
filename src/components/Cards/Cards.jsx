cb1491b1b8ad8a30f100e022385ccba72d1c563d
Pliffisson <pliffissongomes@gmail.com>
2020-04-14 20:42:58 -0400

    install material-ui

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'
 import styles from './Cards.module.css'
 
-export default function Cards(props){
-    console.log(props)
+export default function Cards({ confirmed, recovered, deaths, lastUpdate}){
     return(
         <div className={styles.container}>
             <Grid container spacing={3} justify='center'>

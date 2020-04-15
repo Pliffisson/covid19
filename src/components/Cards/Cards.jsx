374ee5bdd17670db658c766f62459afdc2f9e326
Pliffisson <pliffissongomes@gmail.com>
2020-04-14 20:55:32 -0400

    install material-ui

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'
 import styles from './Cards.module.css'
 
-export default function Cards({ confirmed, recovered, deaths, lastUpdate}){
+const Cards = ({ confirmed, recovered, deaths, lastUpdate}) => {
+    if(!confirmed){
+        return 'Loading ...'
+    }
+
     return(
         <div className={styles.container}>
             <Grid container spacing={3} justify='center'>
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>Infected</Typography>
-                        <Typography variant='h5'>REAL DATA</Typography>
+                        <Typography variant='h5'>{confirmed.value}</Typography>
                         <Typography color='textSecodary'>REAL DATA</Typography>
                         <Typography variant='body2'>Number of active cases of COVID-19</Typography>
                     </CardContent>
@@ -34,3 +38,4 @@ export default function Cards({ confirmed, recovered, deaths, lastUpdate}){
         </div>
     )
 }
+export default Cards;
